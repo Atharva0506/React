626fac566e93a4b79db62a49b99a4d54938138a4
Atharva Naik <atharvan.coder@gmail.com>
2024-10-02 20:34:05 +0530

    themes

--- React Js/vite-project/src/App.tsx
+++ React Js/vite-project/src/App.tsx
@@ -1,33 +1,20 @@
-
-
-import {  useState } from 'react';
-import { Product } from './components/Product';
-import ThemeSwitcher from './components/ThemeSwitcher';
-import { ThemeContextProvider } from './context/TheamContext';
+import { useContext } from "react";
+import { Product } from "./components/Product";
+import ThemeSwitcher from "./components/ThemeSwitcher";
+import { ThemeContext } from "./context/TheamContext";
 
 function App() {
-
-const [time , setTime] = useState("")
-  
- 
-
-    setInterval(() => {
-      const t = new Date()
-      if(t.getMinutes() === Number(52)) {setTime("NO time")}
-      else {
-
-        setTime(String(t.getHours() + ":" + t.getMinutes() + ":" + t.getSeconds() ))
-      }
-    }, 1000);
-  
-
+  const themeContext = useContext(ThemeContext);
+  const theme = themeContext.theme;
   return (
-   <ThemeContextProvider>
-    <div className='text-2xl font-bold flex justify-center m-4'>{time}</div>
-    <ThemeSwitcher/>
-   {/* <Product/> */}
-   </ThemeContextProvider>
+ 
+      <div
+        className={`h-full w-full bg-[${theme.backgroundColor}] text-[${theme.color}] backdrop-blur-lg`}   >
+        <ThemeSwitcher />
+        <Product />
+      </div>
+ 
   );
 }
 
-export default App
+export default App;
--- React Js/vite-project/src/context/TheamContext.tsx
+++ React Js/vite-project/src/context/TheamContext.tsx
@@ -5,26 +5,29 @@ import {
   ThemeName,
   ThemeProviderProps,
 } from "../interface/Theam.interface";
-
-export const ThemeContext = createContext<ThemeContextType | undefined>(
-  undefined
+const themes: { [key in ThemeName]: Theme } = {
+  light: {
+    backgroundColor: "#ffffff",
+    color: "#000000",
+  },
+  dark: {
+    backgroundColor: "#000000",
+    color: "#ffffff",
+  },
+  blue: {
+    backgroundColor: "#87CEEB",
+    color: "#ffffff",
+  },
+};
+export const ThemeContext = createContext<ThemeContextType >(
+  {
+    theme: themes.light,
+    switchTheme: () => {}
+  }
 );
 
 export const ThemeContextProvider = ({ children }: ThemeProviderProps) => {
-  const themes: { [key in ThemeName]: Theme } = {
-    light: {
-      backgroundColor: "#ffffff",
-      color: "#000000",
-    },
-    dark: {
-      backgroundColor: "#000000",
-      color: "#ffffff",
-    },
-    blue: {
-      backgroundColor: "#87CEEB",
-      color: "#ffffff",
-    },
-  };
+ 
   const [theme, setTheme] = useState<Theme>(themes.light);
 
   const switchTheme = (themeName: ThemeName) => {
